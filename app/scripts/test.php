0629c7e122f1ca403788e77b00cda49980df191b
baudeveloper <webmaster@baustein.ca>
2017-05-15 17:18:35 -0400

    Added headers in mail() function.

--- app/scripts/test.php
+++ app/scripts/test.php
@@ -5,9 +5,14 @@
         $company_name = filter_var($_POST['company_name'], FILTER_SANITIZE_STRING);
         $email_address = $_POST['email_address'];
         $requirements = $_POST['requirements'];
-        $to = "test@baustein.ca";
-        $subject = "From $first_name -- Baustein Landing Page";
-        $message = "Thanks, A form has been submitted.\nName: ".$first_name." ".$last_name."\nCompany Name: ".$company_name."\nEmail Address: ".$email_address."\nMessage: ".$requirements;
-        mail($to, $subject, $message);
+        $headers = 'From: webmaster@baustein.ca' . "\r\n" .
+                   'Reply-To: webmaster@baustein.ca' . "\r\n" .
+                   'Content-Type: text/html; charset=ISO-8859-1' . "\r\n" .
+                   'X-Mailer: PHP/' . phpversion();
+        $to = "baustein.design@gmail.com";
+        // $to = "test@baustein.ca";
+        $subject = "New form submission from Wealth Management Landing page";
+        $message = "This mail is to notify you that <b>".$first_name." ".$last_name."</b> recently submitted a form on Baustein's Wealth Management landing page.<hr>Name: ".$first_name." ".$last_name."<br>Company Name: ".$company_name."<br>Email Address: ".$email_address."<br>Message: ".$requirements;
+        mail($to, $subject, $message, $headers);
     }
 ?>
