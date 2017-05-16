5dbd1f74ee896ff04c1a57138fb7e4b87956441c
baudeveloper <webmaster@baustein.ca>
2017-05-16 15:11:25 -0400

    Added Executive Director - David Taylor's address as recipient.

--- app/scripts/test.php
+++ app/scripts/test.php
@@ -9,8 +9,8 @@
                    'Reply-To: webmaster@baustein.ca' . "\r\n" .
                    'Content-Type: text/html; charset=ISO-8859-1' . "\r\n" .
                    'X-Mailer: PHP/' . phpversion();
-        $to = "baustein.design@gmail.com";
-        // $to = "test@baustein.ca";
+        $to = "baustein.design@gmail.com, davidt@baustein.ca";
+        // $to = "billgates@gmail.com, linustorvalds@gmail.com";
         $subject = "New form submission from Wealth Management Landing page";
         $message = "This mail is to notify you that <b>".$first_name." ".$last_name."</b> recently submitted a form on Baustein's Wealth Management landing page.<hr>Name: ".$first_name." ".$last_name."<br>Company Name: ".$company_name."<br>Email Address: ".$email_address."<br>Message: ".$requirements;
         mail($to, $subject, $message, $headers);
